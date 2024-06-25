07807ca318bc8c212f128f643f1da92febd2dbe7
Steven Cao <ffb19b4b-9b78-4683-a582-170da689aa7f>
2024-06-25 00:15:28 +0000

    commit

---
+++ outbound_delivery_list_ic/ext/FormatterExtension.js
@@ -0,0 +1,72 @@
+sap.ui.define([
+    "sap/ui/integration/Extension"
+], 
+    function (Extension) {
+        "use strict";
+        var CardExtension = Extension.extend("winnings.outbound_delivery_list_ic.ext.FormatterExtension");
+
+        CardExtension.prototype.getData = function (arg) {
+            
+            var oCard = this.getCard();
+
+            let sGreaterThanDate;
+
+            switch (arg) {
+                case "0" :
+                        sGreaterThanDate = (moment().subtract(7, 'days').toISOString()).slice(0, -1);
+                        break;
+
+                case "1" : 
+                        sGreaterThanDate = (moment().subtract(14, 'days').toISOString()).slice(0, -1);
+                        break;
+
+                case "2" :
+                        sGreaterThanDate = (moment().subtract(30, 'days').toISOString()).slice(0, -1);
+                        break;
+            }
+
+            let sFilterString = "";
+
+            if (sGreaterThanDate){
+                sFilterString = "DeliveryDate gt datetime'" + sGreaterThanDate + "'";
+            }
+
+            if (!sFilterString) {
+                return new Promise((resolve, reject) => {
+                    resolve([]);
+                })
+            } else {
+                console.log(sFilterString);
+                return oCard.request({
+                    "url" : "{{destinations.ds4}}/sap/opu/odata/sap/API_OUTBOUND_DELIVERY_SRV;v=2/A_OutbDeliveryHeader?$filter=" + sFilterString + "&$format=json",
+                    "withCredentials": true,
+                    "parameters" : {
+                        "$expand" : "to_DeliveryDocumentItem,to_DeliveryDocumentPartner/to_Address"
+                    }
+                }).then(function(oData){
+                    let results = oData.d.results;
+                    console.log(results);
+                    results.forEach(x => {
+                        const date = new Date(parseInt(x.DeliveryDate.substr(6)));
+                        x.DeliveryDateDisp = date.toLocaleString().substring(0, 10);
+                    });
+    
+                    return results;
+                })
+            }
+        };
+
+        CardExtension.prototype.getQuantityFormatter = function (val) {
+            return parseInt(val);
+        };
+        
+        CardExtension.prototype.getFormatters = function() {
+            return {
+                getData: this.getData,
+                getQuantityFormatter: this.getQuantityFormatter
+            }
+        };
+    
+        return CardExtension;
+    }
+); 
